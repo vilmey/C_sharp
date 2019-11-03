d1feb94723252c052f3eccdf4fa938ea1af593d4
Vilmey <vilmeyr@gmail.com>
2019-11-04 00:44:34 +0800

    File manipulation excerxcise.

---
+++ Classes/Files/Files/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\TestFile");
+            //File.Create(Directory.GetCurrentDirectory() + "\\TestFile" + "text.txt");
+            File.WriteAllText(Directory.GetCurrentDirectory() + "\\TestFile" + "text.txt", "This is the text inside the file.\nAnd maybe I should write a verry long word like anthropology.");
+            var words = File.ReadAllText(Directory.GetCurrentDirectory() + "\\TestFile" + "text.txt").Split(" ");
+
+            int indexOfLongestWord = 0;
+            int sizeOfLongestWord = 0;
+            Console.WriteLine("File content:");
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > sizeOfLongestWord)
+                {
+                    sizeOfLongestWord = words[i].Length;
+                    indexOfLongestWord = i;
+                }
+                
+                Console.WriteLine(words[i] + " ");
+            }
+            
+            Console.WriteLine("Number of words: [{0}].", words.Length);
+            Console.WriteLine("Longest word with {0} characters is {1}.", sizeOfLongestWord, words[indexOfLongestWord]);
+        }
+    }
+}
